6c0c1aec23572aa3701ab2396e5a39dcdf32253f
lake <posuihushui@icloud.com>
2024-12-17 10:31:36 +0800

    add backbutton

---
+++ packages/the-open-layer-fe/src/components/ux/backButton.tsx
@@ -0,0 +1,29 @@
+import WebApp from '@twa-dev/sdk';
+import { memo, useEffect, useRef } from 'react';
+
+export const ButtonBack = memo(({ onClick }: { onClick: () => void }) => {
+  const onClickRef = useRef(onClick);
+  useEffect(() => {
+    onClickRef.current = onClick;
+  }, [onClick]);
+
+  useEffect(() => {
+    const forceHide = () => WebApp.BackButton.hide();
+    window.addEventListener('beforeunload', forceHide);
+    return () => window.removeEventListener('beforeunload', forceHide);
+  }, []);
+
+  useEffect(() => {
+    const handleBackButtonClick = () => {
+      onClickRef.current();
+    };
+    WebApp.BackButton.show();
+    WebApp.BackButton.onClick(handleBackButtonClick);
+
+    return () => {
+      WebApp.BackButton.hide();
+      WebApp.BackButton.offClick(handleBackButtonClick);
+    };
+  }, []);
+  return null;
+});
